7879c6f502c0c35f16841b5be5ac53e7aa7b624f
SantamRC <santamdev404@gmail.com>
2022-05-10 01:46:39 +0530

    Created a server file

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv')
+const express=require('express')
+const app = express()
+const port= 8080 || process.env.PORT
+
+app.use(express.json())
+
+app.listen(port,()=>{
+    console.log('Listening on Port '+port)
+})
