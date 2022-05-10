1087052221bccd1f88629f4a9024a34d633ea443
SantamRC <santamdev404@gmail.com>
2022-05-10 21:26:08 +0530

    Added route

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express=require('express')
 const app = express()
 const port= 8080 || process.env.PORT
 
-app.use(express.json())
+app.use(express.json()) 
+
+app.get("/", (req, res) => {
+  res.send("<h1>Working App </h1>");
+});
 
 app.listen(port,()=>{
     console.log('Listening on Port '+port)
