ca3e6fb738eeea86527c5a0f7840ae71b82d1a01
SantamRC <santamdev404@gmail.com>
2022-05-10 21:47:23 +0530

    Initialized Stream

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 require('dotenv')
 const express=require('express')
 const app = express()
-const port= 8080 || process.env.PORT
+const path = require("path");
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
+const port = 8080 || process.env.PORT;
 
-app.use(express.json()) 
+app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Working App </h1>");
 });
 
-app.listen(port,()=>{
-    console.log('Listening on Port '+port)
-})
+const conn = mongoose.createConnection(process.env.MONGODB_URI);
+
+conn.once("open", () => {
+  var gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("cms");
+});
+
+app.listen(port, () => {
+  console.log("Listening on Port " + port);
+}); 
